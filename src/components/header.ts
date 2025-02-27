91830092be938e9aad16d0d81390b97ec8399217
Isabel Sáenz <isasaete@gmail.comn>
2025-02-27 13:31:34 +0100

    Modify header.ts

--- src/components/header.ts
+++ src/components/header.ts
@@ -1,10 +1,9 @@
 export const renderHeader = () => {
   const header = document.createElement("header");
-
+  header.classList.add("main-header");
   const currentUrl = new URL(window.location.href);
 
   header.innerHTML = `
-    <header class="main-header">
       <div class="container">
         <h1>Classroom</h1>
         <nav class="main-navigation">
@@ -25,7 +24,7 @@ export const renderHeader = () => {
           </ul>
         </nav>
       </div>
-    </header>
+   
   `;
 
   document.body.prepend(header);
