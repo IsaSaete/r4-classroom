03667a071b454e170717abdb1005f59637a4085f
Isabel Sáenz <isasaete@gmail.comn>
2025-03-01 09:54:35 +0100

    Complete Feature 21

--- src/grades/service/gradesService.ts
+++ src/grades/service/gradesService.ts
@@ -9,13 +9,30 @@ export const getGradesTotal = (grades: Grade[]): number => grades.length;
 // La función debe recibir una nota
 // La función debe devolver un objeto con las mismas propiedades de la nota
 // más las propiedades studentName, studentLastName y courseName
-// export const getGradeFullData =
+// export const getGradeFullData
 
 // Crea una función para eliminar una nota de la lista de notas
 // La función debe recibir un array de notas y el id de la nota a eliminar
 // export const deleteGrade =
 
-// Crea una función para crear una nueva nota
-// La función debe recibir un array de notas, el id del estudiante, el id del curso y el valor de la nota
-// Si la nota ya existe, muestra un error con showErrorModal
-// export const addGrade =
+export const addGrade = (
+  grades: Grade[],
+  idStudent: number,
+  idCourse: number,
+  gradeValue: number
+): void => {
+  const isGradeExist = grades.some(
+    (grade) => grade.studentId === idStudent && grade.courseId === idCourse
+  );
+  if (isGradeExist) {
+    showErrorModal("Esta nota ya está creada");
+    return;
+  }
+  const newGrade = {
+    id: generateId(grades),
+    studentId: idStudent,
+    courseId: idCourse,
+    value: gradeValue,
+  };
+  grades.push(newGrade);
+};
